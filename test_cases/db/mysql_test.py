bfa107277362c8e83a8cf0291fcf5437fa9d0de4
tilmitt11191 <tilmitt11@yahoo.co.jp>
2017-04-04 23:03:29 +0900

    db

--- test_cases/db/mysql_test.py
+++ test_cases/db/mysql_test.py
@@ -11,16 +11,19 @@ class MySQL_test(unittest.TestCase):
 		sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../lib/utils")
 		from log import Log as l
 		cls.log = l().getLogger()
+		sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../lib/db")
+		import mysql_operator
+		cls.db = mysql_operator.Mysql_operator()
 
 		cls.log.info("\n\nMySQL_test.setUpClass finished.\n---------- start ---------")
 	
 	def setUp(self):
-		import sqlalchemy
-		self.engine = sqlalchemy.create_engine("mysql+pymysql://alladmin:admin@localhost/paper_graph?charset=utf8", echo=False)
-		from sqlalchemy.orm import sessionmaker
-		Session = sessionmaker(bind=self.engine)
-		self.session = Session()
-
+		#import sqlalchemy
+		#self.engine = sqlalchemy.create_engine("mysql+pymysql://alladmin:admin@localhost/paper_graph?charset=utf8", echo=False)
+		#from sqlalchemy.orm import sessionmaker
+		#Session = sessionmaker(bind=self.engine)
+		#self.db.session = Session()
+		pass
 	
 	def get_titles_authors_from_deeplearningtraffic(self):
 		self.log.info(__class__.__name__ + "." + sys._getframe().f_code.co_name + " start")
@@ -41,7 +44,7 @@ class MySQL_test(unittest.TestCase):
 		self.log.info(__class__.__name__ + "." + sys._getframe().f_code.co_name + " finished")
 		return titles, authors
 		
-	
+	"""
 	def test_insert_a_title(self):
 		self.log.info(__class__.__name__ + "." + sys._getframe().f_code.co_name + " start")
 		title = "Behind the Game: Exploring the Twitch Streaming Platform"
@@ -50,8 +53,8 @@ class MySQL_test(unittest.TestCase):
 		timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
 		sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../lib/db")
 		import table_papers
-		initial_num = self.session.query(table_papers.Table_papers).count()
-		self.assertEqual(initial_num, self.session.query(table_papers.Table_papers).count())
+		initial_num = self.db.session.query(table_papers.Table_papers).count()
+		self.assertEqual(initial_num, self.db.session.query(table_papers.Table_papers).count())
 		
 		paper = table_papers.Table_papers(
 			id=1,
@@ -59,13 +62,75 @@ class MySQL_test(unittest.TestCase):
 			timestamp=timestamp
 		)
 		#print(paper.get_vars())
-		self.assertEqual(initial_num, self.session.query(table_papers.Table_papers).count())
-		self.session.add(paper)
-		self.assertEqual(initial_num + 1, self.session.query(table_papers.Table_papers).count())
-		self.session.commit()
+		self.assertEqual(initial_num, self.db.session.query(table_papers.Table_papers).count())
+		self.db.session.add(paper)
+		self.assertEqual(initial_num + 1, self.db.session.query(table_papers.Table_papers).count())
+		self.db.session.commit()
 		
 		self.log.info(__class__.__name__ + "." + sys._getframe().f_code.co_name + " finished")
+	"""
+	"""
+	def test_insert_a_title2(self):
+		self.log.info(__class__.__name__ + "." + sys._getframe().f_code.co_name + " start")
+		title = "Behind the Game: Exploring the Twitch Streaming Platform"
 
+		import time
+		timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+		sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../lib/db")
+		import table_papers
+		initial_num = self.db.session.query(table_papers.Table_papers).count()
+
+		paper = table_papers.Table_papers(
+			id=1,
+			title=title,
+			timestamp=timestamp
+		)
+		
+		self.db.insert(paper)
+		self.assertEqual(initial_num+1, self.db.session.query(table_papers.Table_papers).count())
+		
+		
+		self.log.info(__class__.__name__ + "." + sys._getframe().f_code.co_name + " finished")
+	"""
+	def test_delete_a_title(self):
+		self.log.info(__class__.__name__ + "." + sys._getframe().f_code.co_name + " start")
+		title = "Behind the Game: Exploring the Twitch Streaming Platform"
+		
+		import table_papers
+		paper = table_papers.Table_papers(title=title)
+		
+		#self.db.delete(paper)
+		
+		import time
+		timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+		paper = table_papers.Table_papers(
+			id=1,
+			title=title,
+			timestamp=timestamp
+		)
+		
+		#self.db.insert(paper)
+		paper = table_papers.Table_papers(
+			id=1,
+			title=title,
+			timestamp=timestamp
+		)
+		self.db.insert(paper)
+		self.db.delete_by_id_from_papers(1)
+		
+		self.log.info(__class__.__name__ + "." + sys._getframe().f_code.co_name + " finished")
+	
+	
+	"""
+	def test_init_mysql_operator(self):
+		self.log.info(__class__.__name__ + "." + sys._getframe().f_code.co_name + " start")
+		sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../lib/db")
+		import mysql_operator
+		self.db = mysql_operator.Mysql_operator()
+		self.log.info(__class__.__name__ + "." + sys._getframe().f_code.co_name + " finished")
+	"""
+	
+	
 if __name__ == '__main__':
 	unittest.main()
 
