8fa1771e5698ad5fae65e336b7d98add29f86860
tilmitt11191 <tilmitt11@yahoo.co.jp>
2017-05-07 16:59:50 +0900

    2017年 5月  7日 日曜日 16:59:42 JST

--- lib/scraping/restart_searching.py
+++ lib/scraping/restart_searching.py
@@ -14,6 +14,9 @@ timeout=30
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../lib/math")
 from searchs import Searchs
 
+from IEEEXplore import IEEEXplore
+xplore = IEEEXplore()
+
 search = Searchs.restore_status(saved_file)
 print("node: " + search.node)
 print("limit: " + str(search.limit))
