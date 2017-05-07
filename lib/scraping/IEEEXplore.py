842e968d18c65fa407c9e64ca5ea688c338a119e
tilmitt11191 <tilmitt11@yahoo.co.jp>
2017-05-07 17:12:20 +0900

    2017年 5月  7日 日曜日 17:12:12 JST

--- lib/scraping/IEEEXplore.py
+++ lib/scraping/IEEEXplore.py
@@ -101,11 +101,11 @@ class IEEEXplore:
 		paper.timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
 		if filename == "title":
 			filename = paper.title + ".pdf"
-		#paper.path = self.download_a_paper(
-		#	driver, path=path, filename=filename, timeout=timeout)
-		#self.log.debug("download finished. wait start.")
-		#time.sleep(self.conf.getconf("IEEE_wait_time_per_download_paper"))
-		#self.log.debug("wait finished.")
+		paper.path = self.download_a_paper(
+			driver, path=path, filename=filename, timeout=timeout)
+		self.log.debug("download finished. wait start.")
+		time.sleep(self.conf.getconf("IEEE_wait_time_per_download_paper"))
+		self.log.debug("wait finished.")
 		paper.id = paper.get_id()
 
 		self.log.debug(paper.get_vars())
