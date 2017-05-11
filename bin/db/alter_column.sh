6be5a62b698c791f85b42f39f65094156837c432
tilmitt11191 <tilmitt11@gmail.com>
2017-05-11 10:51:17 +0900

    2017年 5月 11日 木曜日 10:51:15 JST

--- bin/db/alter_column.sh
+++ bin/db/alter_column.sh
@@ -1,4 +1,6 @@
 
 
 #ALTER TABLE tbl_name CHANGE [COLUMN] old_col_name column_definition;
-alter table paper_graph.papers change path text;
+sudo mysql -p -e "\
+alter table paper_graph.papers change path path text;\
+flush privileges;"
